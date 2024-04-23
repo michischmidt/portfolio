c70b5bcb9e2cbac01a8a051d92d4e321d4afed10
Michael Schmidt <schmidt.m.dev@gmail.com>
2024-04-23 20:32:57 +0200

    added i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,123 @@
+export const languages = {
+	en: 'English',
+	de: 'Deutsch'
+}
+
+export const defaultLang = 'en'
+
+export const ui = {
+	en: {
+		'contact.location': 'Germany, Regensburg',
+		'abstract.text':
+			'Passionate Full-Stack Software Developer with over 5 years of experience. Enthusiastic about working on challenging projects with experts, learning new technologies, and gaining practical knowledge.',
+		'exp.title': 'Experience',
+		'exp.gg': 'Grow Ghana EDU - Ghana, Kokrobite',
+		'exp.gg.job': 'Volunteer & Teacher',
+		'exp.gg.date': 'Dec 2023 - Mar 2024',
+		'exp.gg.point1':
+			'Teaching young underprivileged students in in Ghana about basic computer skills with Scratch, soft skills and beginner Python courses.',
+		'exp.gg.point2': 'Supporting the local team in mangaging the daily NGO business.',
+		'exp.trifull': 'trinnovative GmbH - Germany, Regensburg',
+		'exp.trifull.job': 'Software Engineer & IT-Consultant',
+		'exp.trifull.date': 'Apr 2022 - Dec 2023',
+		'exp.trifull.point1':
+			'Developing a custom React component library with Tailwind CSS and implementing a web portal for multiple stakeholders for a customer.',
+		'exp.trifull.point2':
+			'Designing, coordinating, and implementing an internal intranet for existing and new self-services using Figma, Kubernetes, Azure, Turborepo, React, and Material UI.',
+		'exp.trifull.point3':
+			'Holding interal and external trainings for developers in React and Distributed Systems.',
+		'exp.trifull.point4':
+			'Implementing and updating a React frontend with Material UI for a generic logisctics depostion software for a customer.',
+		'exp.tri': 'trinnovative GmbH - Germany, Regensburg',
+		'exp.tri.job': 'Working Student',
+		'exp.tri.date': 'Apr 2019 - Apr 2022',
+		'exp.tri.point1':
+			"Researching, designing, and implementing my Bachelor's thesis for a cost-effective multi-tenant SaaS solution for a generic booking app for AWS using Adobe XD, AWS services, Serverless architecture, React, Material UI and Express.",
+		'exp.tri.point2':
+			'Supporting the team in external projects frontend development with React and Material UI.',
+		'exp.tri.point3': 'Updating the subscription payment of a React frontend using Stripe.',
+		'exp.daimler': 'Daimler AG - Germany, Stuttgart',
+		'exp.daimler.job': 'App Development Intern',
+		'exp.daimler.date': 'Aug 2018 - Jan 2019',
+		'exp.daimler.point1':
+			"Creation of a PoC API for push notifications for an internal employee self-service app using Java, Firebase and interal API's.",
+		'exp.daimler.point2':
+			'Supporting the team in feature development, quality analysis and communication between departures.',
+		'edu.title': 'Education',
+		'edu.uni': 'University of Regensburg',
+		'edu.uni.degree': 'Master of Science in Media Computer Science (On hold)',
+		'edu.uni.date': 'Dec 2022 - Nov 2023',
+		'edu.oth': 'University of Applied Sciences of Regensburg',
+		'edu.oth.degree': 'Bachelor of Science in Computer Science',
+		'edu.oth.date': 'Dec 2022 - Nov 2023',
+		'skills.title': 'Skills',
+		'skills.frontend': 'Frontend',
+		'skills.languages': 'Languages',
+		'skills.database': 'Database',
+		'skills.programming': 'Programming',
+		'skills.others': 'Others',
+		'projects.title': 'Projects',
+		'projects.fs.title': 'Frontstart - React Trainign',
+		'projects.fs.text':
+			'Landing Page for React Training Programm I created. Build with Next.js, React, Tailwind and hosted on Vercel.',
+		'certs.title': 'Certifications',
+		'certs.oose': 'oose iSQB Qualification - Germany, Hamburg'
+	},
+	de: {
+		'contact.location': 'Deutschland, Regensburg',
+		'abstract.text':
+			'Leidenschaftlicher Full-Stack-Softwareentwickler mit über 5 Jahren Erfahrung. Begeistert von der Arbeit an anspruchsvollen Projekten mit Experten, dem Erlernen neuer Technologien und dem Erwerb praktischer Kenntnisse.',
+		'exp.title': 'Erfahrung',
+		'exp.gg': 'Grow Ghana EDU - Ghana, Kokrobite',
+		'exp.gg.job': 'Freiwilliger & Lehrer',
+		'exp.gg.date': 'Dez 2023 - Mär 2024',
+		'exp.gg.point1':
+			'Unterricht junger unterpriviligierter Schüler in Ghana zu grundlegenden Computerkenntnissen mit Scratch, Soft Skills und Einsteiger-Python-Kursen.',
+		'exp.gg.point2': 'Unterstützung des örtlichen Teams bei der täglichen NGO-Geschäftsführung.',
+		'exp.trifull': 'trinnovative GmbH - Deutschland, Regensburg',
+		'exp.trifull.job': 'Softwareentwickler & IT-Berater',
+		'exp.trifull.date': 'Apr 2022 - Dez 2023',
+		'exp.trifull.point1':
+			'Entwicklung einer benutzerdefinierten React Komponentenbibliothek mit Tailwind CSS und Implementierung eines Webportals für mehrere Stakeholder für einen Kunden.',
+		'exp.trifull.point2':
+			'Entwurf, Koordination und Implementierung eines internen Intranets für bestehende und neue Self-Services mit Figma, Kubernetes, Azure, Turborepo, React und Material UI.',
+		'exp.trifull.point3':
+			'Durchführung interner und externer Schulungen für Entwickler in React und verteilten Systemen.',
+		'exp.trifull.point4':
+			'Implementierung und Aktualisierung eines React-Frontends mit Material UI für eine generische Logistik-Depositionssoftware für einen Kunden.',
+		'exp.tri': 'trinnovative GmbH - Deutschland, Regensburg',
+		'exp.tri.job': 'Werkstudent',
+		'exp.tri.date': 'Apr 2019 - Apr 2022',
+		'exp.tri.point1':
+			'Recherche, Design und Implementierung meiner Bachelorarbeit für eine kostengünstige Multi-Tenant-SaaS-Lösung für eine generische Buchungs-App für AWS unter Verwendung von Adobe XD, AWS-Services, Serverless-Architektur, React, Material UI und Express.',
+		'exp.tri.point2':
+			'Unterstützung des Teams bei der Frontend-Entwicklung externer Projekte mit React und Material UI.',
+		'exp.tri.point3': 'Aktualisierung der Abonnementsystems eines React Frontends mit Stripe.',
+		'exp.daimler': 'Daimler AG - Deutschland, Stuttgart',
+		'exp.daimler.job': 'Praktikant in der App-Entwicklung',
+		'exp.daimler.date': 'Aug 2018 - Jan 2019',
+		'exp.daimler.point1':
+			'Erstellung einer PoC-API für Push-Benachrichtigungen für eine interne Mitarbeiter-Self-Service-App unter Verwendung von Java, Firebase und internen APIs.',
+		'exp.daimler.point2':
+			'Unterstützung des Teams bei der Feature-Entwicklung, Qualitätsanalyse und Kommunikation zwischen den Abteilungen.',
+		'edu.title': 'Ausbildung',
+		'edu.uni': 'Universität Regensburg',
+		'edu.uni.degree': 'Master of Science in Medieninformatik (On hold)',
+		'edu.uni.date': 'Dez 2022 - Nov 2023',
+		'edu.oth': 'Hochschule Regensburg',
+		'edu.oth.degree': 'Bachelor of Science in Informatik',
+		'edu.oth.date': 'Dez 2022 - Nov 2023',
+		'skills.title': 'Fähigkeiten',
+		'skills.frontend': 'Frontend',
+		'skills.languages': 'Sprachen',
+		'skills.database': 'Datenbank',
+		'skills.programming': 'Programmieren',
+		'skills.others': 'Andere',
+		'projects.title': 'Projekte',
+		'projects.fs.title': 'Frontstart - React-Training',
+		'projects.fs.text':
+			'Landing Page für das von mir erstellte React Trainingsprogramm. Entwickelt mit Next.js, React, Tailwind und gehostet auf Vercel.',
+		'certs.title': 'Zertifizierungen',
+		'certs.oose': 'oose iSQB Qualifikation - Deutschland, Hamburg'
+	}
+} as const
