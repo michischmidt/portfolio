fa304c56b30eb1f27a01dc9abf33f66eb6f29b12
Michael Schmidt <schmidt.m.dev@gmail.com>
2024-04-29 14:15:56 +0200

    added imprint

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -7,6 +7,18 @@ export const defaultLang = 'en'
 
 export const ui = {
 	en: {
+		imprint: 'Impressum',
+		'imprint.agb': 'Terms and Conditions and Disclaimer',
+		'imprint.responsesiblity': 'Content Responsibility',
+		'imprint.responsesiblity.text':
+			'We take great care to ensure the accuracy and timeliness of the content presented on our website. However, we do not guarantee its completeness, correctness, or timeliness. As a service provider, we are responsible for our own content on these pages under general laws pursuant to Section 7(1) of the German Telemedia Act (TMG). However, according to §§ 8 to 10 TMG, we are not obligated to monitor transmitted or stored third-party information or to investigate circumstances that indicate illegal activity. Should we become aware of any legal violations, we will remove such content immediately. Liability is only possible from the point in time at which we become aware of a specific violation.',
+		'imprint.links': 'Liability for External Links',
+		'imprint.links.text':
+			'Our website contains links to external websites of third parties, the content of which is beyond our control. The respective provider or operator is always responsible for the content and accuracy of the linked pages. At the time of linking, these external pages were checked for possible legal violations; illegal content was not apparent at that time. However, ongoing monitoring of the content of linked pages without concrete evidence is not reasonable. If we become aware of legal violations, we will remove such links from our website immediately.',
+		'imprint.copy': 'Copyright and Performance Rights',
+		'imprint.copy.text':
+			'The content and works created by the website operator on this website are subject to German copyright law. The reproduction, editing, distribution, and any kind of exploitation beyond the limits of copyright law require the prior written consent of the respective author or creator. Downloads and copies of this site are only permitted for private, non-commercial use. If content on this site was not created by the operator, the copyrights of third parties are respected and such content is identified as such. If you become aware of a copyright infringement, please notify us accordingly. Upon becoming aware of legal violations, we will remove such content immediately.',
+
 		'contact.location': 'Germany, Regensburg',
 		'abstract.text':
 			'Passionate Full-Stack Software Developer with over 5 years of experience. Enthusiastic about working on challenging projects with experts, learning new technologies, and gaining practical knowledge.',
@@ -45,7 +57,7 @@ export const ui = {
 			'Supporting the team in feature development, quality analysis and communication between departures.',
 		'edu.title': 'Education',
 		'edu.uni': 'University of Regensburg',
-		'edu.uni.degree': 'Master of Science in Medieninformatik (pausiert)',
+		'edu.uni.degree': 'Master of Science in Medieninformatik (on hold)',
 		'edu.uni.date': 'Dec 2022 - Nov 2023',
 		'edu.oth': 'University of Applied Sciences of Regensburg',
 		'edu.oth.degree': 'Bachelor of Science in Informatik',
@@ -64,6 +76,17 @@ export const ui = {
 		'certs.oose': 'oose iSQB Qualification - Germany, Hamburg'
 	},
 	de: {
+		imprint: 'Impressum',
+		'imprint.agb': 'Allgemeine Geschäftsbedingungen und Haftungsausschluss',
+		'imprint.responsesiblity': 'Inhaltliche Verantwortung',
+		'imprint.responsesiblity.text':
+			'Wir legen großen Wert auf die Sorgfalt und Aktualität der auf unserer Webseite präsentierten Inhalte. Dennoch übernehmen wir keine Garantie für deren Vollständigkeit, Korrektheit oder Aktualität. Als Anbieter sind wir gemäß § 7 Abs.1 TMG für die eigenen Inhalte auf unserer Seite nach den allgemeinen Gesetzen verantwortlich, sind jedoch nach §§ 8 bis 10 TMG nicht verpflichtet, übermittelte oder gespeicherte Informationen von Dritten aktiv zu überwachen oder nach Umständen zu forschen, die auf illegale Aktivitäten hinweisen. Sollten wir von Rechtsverletzungen Kenntnis erlangen, werden wir derartige Inhalte umgehend entfernen. Eine Haftung wird jedoch erst ab dem Zeitpunkt der Kenntnis konkreter Verletzungen anwendbar.',
+		'imprint.links': 'Haftung für externe Links',
+		'imprint.links.text':
+			'Unsere Webseite enthält Verlinkungen zu externen Webseiten Dritter, deren Inhalte außerhalb unseres Einflussbereichs liegen. Für die Inhalte und die Richtigkeit der Informationen der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber verantwortlich. Zum Zeitpunkt der Verlinkung wurden diese externen Seiten auf mögliche Rechtsverstöße überprüft; rechtswidrige Inhalte waren zu diesem Zeitpunkt nicht erkennbar. Eine ständige inhaltliche Kontrolle der verlinkten Seiten ohne konkrete Anhaltspunkte ist uns jedoch nicht zumutbar. Bei Kenntnis von Rechtsverletzungen werden derartige Links unverzüglich von unserer Webseite entfernt.',
+		'imprint.copy': 'Urheber- und Leistungsschutzrechte',
+		'imprint.copy.text':
+			'Die auf dieser Webseite durch den Seitenbetreiber erstellten Inhalte und Werke unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Form der Verwertung außerhalb der Grenzen des Urheberrechts bedürfen der vorherigen schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Falls Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter respektiert und derartige Inhalte als solche gekennzeichnet. Sollten Sie auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir solche Inhalte umgehend entfernen.',
 		'contact.location': 'Deutschland, Regensburg',
 		'abstract.text':
 			'Leidenschaftlicher Full-Stack-Softwareentwickler mit über 5 Jahren Erfahrung. Begeistert von der Arbeit an anspruchsvollen Projekten mit Experten, dem Erlernen neuer Technologien und dem Erwerb praktischer Kenntnisse.',
