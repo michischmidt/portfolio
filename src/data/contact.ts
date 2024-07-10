1b7778b7774cac3fabb83a190f40d38a8e41d82f
Michael Schmidt <schmidt.m.dev@gmail.com>
2024-07-10 09:48:48 +0200

    more updates

---
+++ src/data/contact.ts
@@ -0,0 +1,6 @@
+export const contactInfo = {
+	name: 'Michael Schmidt',
+	address: 'Pflanzenmayerstr. 15, 93049 Regensburg',
+	mobile: '(+49)-176-61382505',
+	email: 'schmidt.m.work@gmail.com'
+}
---
+++ src/data/skills.ts
@@ -0,0 +1,24 @@
+const skills = {
+	frontend: [
+		'HTML',
+		'CSS',
+		'React',
+		'Next.js',
+		'TanStack (React) Query',
+		'Tailwind CSS',
+		'Material UI',
+		'Storybook',
+		'React Native',
+		'Jest',
+		'Redux',
+		'Astro.js'
+	],
+	backend: ['REST', 'RPC', 'Node.js', 'Express', 'Spring Boot', 'Serverless Functions'],
+	database: ['SQL', 'PostgreSQL', 'MySQL', 'NoSQL', 'Key/Value Stores', 'DynamoDB'],
+	cloud: ['AWS', 'Azure', 'Firebase', 'Terraform', 'Vercel'],
+	design: ['Figma', 'Canva'],
+	programming: ['TypeScript', 'JavaScript', 'Java', 'Python'],
+	others: ['Git', 'Scrum', 'DevOps', 'CI/CD', 'Docker', 'Atlassian Toolchain', 'Turborepo']
+}
+
+export default skills
