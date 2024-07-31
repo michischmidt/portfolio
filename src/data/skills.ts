975d5860bcaf89f65eba49dd05ed5a0804dba195
michischmidt <schmidt.m.dev@gmail.com>
2024-07-31 09:54:15 +0200

    newer skills

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -11,12 +11,12 @@ const skills = {
 		'Storybook',
 		'Astro.js'
 	],
-	backend: ['REST', 'RPC', 'Node.js', 'Spring Boot', 'Serverless Functions', 'Fast API'],
+	backend: ['REST', 'RPC', 'Node.js', 'Spring Boot', 'Serverless Functions'],
 	database: ['SQL', 'PostgreSQL', 'MySQL', 'NoSQL', 'Key/Value Stores'],
 	cloud: ['Azure', 'AWS', 'Firebase', 'Terraform', 'Vercel'],
 	design: ['Figma', 'Canva'],
 	programming: ['TypeScript', 'JavaScript', 'Java', 'Python'],
-	others: ['Git', 'Scrum', 'DevOps', 'CI/CD', 'Docker', 'Atlassian Toolchain', 'Turborepo', 'Clean Code']
+	others: ['Git', 'Scrum', 'DevOps', 'CI/CD', 'Docker', 'Atlassian Toolchain', 'Monorepo (Turborepo)', 'Clean Code']
 }
 
 export default skills
